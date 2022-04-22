96cfc830a5ab8c563ad3b157907cbc33521ea3e9
adwatbitcoin <100610318+adwatbitcoin@users.noreply.github.com>
2022-04-22 06:49:10 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# Make sure deps are installed
+yarn install
+
+# build
+rm -rf docs/.vuepress/dist
+npm run docs:build
+
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+# if you are deploying to a custom domain
+# echo 'www.github.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:adwatbitcoin/adwatbitcoin.github.io.git master
+cd -
