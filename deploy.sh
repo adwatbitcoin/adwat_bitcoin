f6e19fc92fdcca577a526efd43499162ac430cb9
adwatbitcoin <100610318+adwatbitcoin@users.noreply.github.com>
2022-04-23 06:20:00 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:adwatbitcoin/adwatbitcoin.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:adwatbitcoin/adwat_bitcoin.git master
+#git push -f git@github.com:adwatbitcoin/adwat_bitcoin.git master
 
 cd -
